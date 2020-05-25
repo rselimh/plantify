dac48611afa8e47c5e5a4a2b8990f3d9c255b843
Robert Hill <hill.rws@gmail.com>
2020-05-25 17:57:09 +0200

    fix syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: ‘https://plantify-gs.herokuapp.com/’ }
+  config.action_mailer.default_url_options = { host: 'https://plantify-gs.herokuapp.com/' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
