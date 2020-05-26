e21c88c8df527f61ce5fd8f4a94d84e68ef93a94
lea sordoillet <leasordoillet@gmail.com>
2020-05-26 13:10:33 +0200

    seed file

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+
 gem 'devise'
 
 gem 'autoprefixer-rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,62 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'faker'
+
+puts "Destroy plants"
+Plant.destroy_all if Rails.env.development?
+
+puts "Create plants"
+
+5.times do
+  user = User.create!(
+    email: Faker::Internet.email,
+    password: "123456"
+    )
+  5.times do
+  plant = Plant.new(
+    name: Faker::Company.catch_phrase,
+    description: Faker::Hipster.sentence(word_count: 3),
+    address: Faker::Address.street_address,
+    price_per_day: Faker::Number.number(digits: 2),
+    status: "declined"
+  )
+  plant.user = user
+  plant.save!
+end
+
+end
+
+
+
+
+# 5.times do
+#   Plant.create!(
+#     name: Faker::Company.catch_phrase,
+#     description: Faker::Hipster.sentence(word_count: 3),
+#     address: Faker::Address.street_address,
+#     price_per_day: Faker::Number.number(digits: 2),
+#     status: "approved"
+#   )
+# end
+
+# 5.times do
+#   Plant.create!(
+#     name: Faker::Company.catch_phrase,
+#     description: Faker::Hipster.sentence(word_count: 3),
+#     address: Faker::Address.street_address,
+#     price_per_day: Faker::Number.number(digits: 2),
+#     status:"pending"
+#   )
+# end
+
+
+puts 'Finished!'
+
+
+      # t.string :name
+      # t.text :description
+      # t.text :address
+      # t.integer :price_per_day
+      # t.string :status "pending" "approuved" "declined"
